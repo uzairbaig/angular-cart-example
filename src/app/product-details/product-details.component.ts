489e9ac815de48bb28ce05095ead37a4c9675322
uzairbaig <engr.uzairbaig@gmail.com>
2019-11-08 17:49:43 +0000

    add checkout, shipping functionality

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import {CartService} from '../cart.service'
+
 import { products } from '../products';
 
 @Component({
@@ -11,12 +13,18 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
 
   product;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+   private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];
   });
+  }
+
+
+  addToCart(product) {
+    this.cartService.addToCart(product); 
   }
 
 }
